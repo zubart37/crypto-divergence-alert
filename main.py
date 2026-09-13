62621400d7e857d9eab2638be94644071c422102
zubart37 <pufpufpug@gmail.com>
2026-09-13 17:01:22 +0300

    Add pandas import to main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import time
 from datetime import datetime, timedelta, timezone
 import aiohttp
 import numpy as np
-
+import pandas as pd
 # ====================== AYARLAR ======================
 TIMEFRAME = '15m'
 RSI_PERIOD = 14
